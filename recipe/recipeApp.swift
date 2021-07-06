265b5acfa99681de6b89c51634e6c19609a61795
Adam Stockermans <adamstox@gmail.com>
2021-07-06 14:28:02 -0400

    Initial Commit

---
+++ recipe/recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeApp.swift
+//  recipe
+//
+//  Created by Stox on 2021-07-06.
+//
+
+import SwiftUI
+
+@main
+struct recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
