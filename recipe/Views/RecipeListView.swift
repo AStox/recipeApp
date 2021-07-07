e88b0cb4c628cd76af92e881efe52de86f8dcf1b
Adam Stockermans <adamstox@gmail.com>
2021-07-07 17:27:53 -0400

    Change swiftUI list to scrollview hide navigation bar title and use text element

--- recipe/Views/RecipeListView.swift
+++ recipe/Views/RecipeListView.swift
@@ -14,17 +14,30 @@ struct RecipeListView: View {
     var body: some View {
         
         NavigationView{
-            List(model.recipes) { r in
-                NavigationLink(
-                    destination: RecipeDetailView(recipe: r),
-                    label: {
-                        
-                    
-                HStack(spacing: 20.0) {
-                    Image(r.image).resizable().scaledToFill().frame(width: 50.0, height: 50.0).clipped().cornerRadius(5)
-                    Text(r.name)
-                }})
-            }.navigationBarTitle("All Recipes")
+            
+            VStack (alignment: .leading) {
+                Text("All Recipes")
+                    .bold()
+                    .padding(.top, 40)
+                    .font(.largeTitle)
+                
+                ScrollView {
+                    LazyVStack (alignment: .leading) {
+                        ForEach(model.recipes) { r in
+                            NavigationLink(
+                                destination: RecipeDetailView(recipe: r),
+                                label: {
+                                    
+                                
+                            HStack(spacing: 20.0) {
+                                Image(r.image).resizable().scaledToFill().frame(width: 50.0, height: 50.0).clipped().cornerRadius(5)
+                                Text(r.name)
+                                    .foregroundColor(.black)
+                            }})
+                        }.navigationBarHidden(true)
+                    }
+                }
+            }.padding(.leading)
             
         }
     }
@@ -33,5 +46,6 @@ struct RecipeListView: View {
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
